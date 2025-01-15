ee0a3f2467af6ecd60a09940e52e204acab3847f
Kaixn Zhang <kaixin2022mel@gmail.com>
2025-01-15 09:36:44 +0800

    Init the project structure

---
+++ SpringReactWeb/src/main/java/com/Kevin/SpringReactWeb/SpringReactWebApplication.java
@@ -0,0 +1,13 @@
+package com.Kevin.SpringReactWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactWebApplication.class, args);
+	}
+
+}
